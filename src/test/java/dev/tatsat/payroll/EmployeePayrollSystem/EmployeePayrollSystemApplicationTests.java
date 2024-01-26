58ec8be6f3441c7f49e2b5aeabe0358863c52ab3
Tatsat Sawant <tatsatsawant6@gmail.com>
2024-01-26 17:06:53 +0530

    project initialized

---
+++ src/test/java/dev/tatsat/payroll/EmployeePayrollSystem/EmployeePayrollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tatsat.payroll.EmployeePayrollSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
